1c78cdeb0e0b22a0aca88d11d369c7512a1dd64c
cortezmb <michael.cortez78@gmail.com>
2020-07-02 14:22:59 -0500

    added loops to get all pages with characters and allegiances

--- javascript/got.js
+++ javascript/got.js
@@ -1,28 +1,35 @@
-$(() => {
 
-    var gotCharacters = fetch('https://www.anapioficeandfire.com/api/characters')
-    var gotAllegiances = fetch('https://www.anapioficeandfire.com/api/houses')
+    var gotCharacters = fetch('https://www.anapioficeandfire.com/api/characters');
+    var gotAllegiances = fetch('https://www.anapioficeandfire.com/api/houses');
+    var characterArr = [];
+    var allegianceArr = [];
 
-    Promise.all([gotCharacters, gotAllegiances])
+    for (var a = 1; a < 40; a++){
+
+        characterArr.push(fetch(`https://www.anapioficeandfire.com/api/characters?page=${a}&pageSize=50`));
+    }
+
+    for (var b = 1; b , 40; b++){
+
+        allegianceArr.push(fetch(`https://www.anapioficeandfire.com/api/houses?page=${b}&pageSize=50`))
+    }
+
+    Promise.all(characterArr, allegianceArr)
 
     .then(responses => {
 
-        responses[0].json;
-        responses[1].json;
-
-        return responses;
+        return Promise.all(responses.map((response)=>response.json()))
     })
 
     .then((data) => {
-        console.log(data);
+        console.log(data[0]);
 
         let nameHtml = data.map(nameObj => {
-
-            return `<li> ${nameObj.name}</li>`
+            console.log(nameObj);
+            return `<li>${nameObj.name}</li>`
         })
 
         let ul = document.querySelector('ul');
 
-        ul.innerHTML = nameHtml.arr
+        ul.innerHTML = nameHtml.arr;
       })
-})
