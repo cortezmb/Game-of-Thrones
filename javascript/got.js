6b8de9262e2bc221885d5c0163b3344035db82b3
cortezmb <michael.cortez78@gmail.com>
2020-07-02 17:10:42 -0500

    list of characters are now working.

--- javascript/got.js
+++ javascript/got.js
@@ -1,35 +1,39 @@
+$(()=> {
 
-    var gotCharacters = fetch('https://www.anapioficeandfire.com/api/characters');
-    var gotAllegiances = fetch('https://www.anapioficeandfire.com/api/houses');
     var characterArr = [];
     var allegianceArr = [];
 
     for (var a = 1; a < 40; a++){
 
-        characterArr.push(fetch(`https://www.anapioficeandfire.com/api/characters?page=${a}&pageSize=50`));
+        characterArr.push(fetch(`https://www.anapioficeandfire.com/api/characters?page=${a}&pageSize=50`));    
     }
 
-    for (var b = 1; b , 40; b++){
+    // for (var b = 1; b < 40; b++){
 
-        allegianceArr.push(fetch(`https://www.anapioficeandfire.com/api/houses?page=${b}&pageSize=50`))
-    }
-
-    Promise.all(characterArr, allegianceArr)
+    //     allegianceArr.push(fetch(`https://www.anapioficeandfire.com/api/houses?page=${b}&pageSize=45`));
+    // }    
+    //, allegianceArr
+    Promise.all(characterArr) 
 
     .then(responses => {
 
         return Promise.all(responses.map((response)=>response.json()))
     })
-
+    
     .then((data) => {
-        console.log(data[0]);
 
-        let nameHtml = data.map(nameObj => {
+        console.log(data)
+        var newArr = data.reduce(nameObj, [])
+        function nameObj(total, name){
+            return total.concat(name)
+        }
+
+        var html = newArr.map(nameObj => {
             console.log(nameObj);
-            return `<li>${nameObj.name}</li>`
+            return `<li>${nameObj.name}</li>`;
         })
-
-        let ul = document.querySelector('ul');
-
-        ul.innerHTML = nameHtml.arr;
-      })
+        let ul = document.getElementById('name');
+        ul.innerHTML = html;
+    })
+})
+   
